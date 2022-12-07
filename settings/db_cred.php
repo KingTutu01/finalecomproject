d986d17aac65b13dea194fe6dd9ba5cadec16cf6
KingTutu01 <kwame.osei-tutu@ashesi.edu.gh>
2022-12-07 22:06:42 +0000

    uploading files

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,8 +1,18 @@
 <?php
 //Database credentials
-define("DATABASE", "shoppn");
-define("SERVER", "localhost");
-define("USERNAME", "root");
-define("PASSWD", "");
+// define("DATABASE", "shoppn");
+// define("SERVER", "localhost");
+// define("USERNAME", "root");
+// define("PASSWD", "");
+
+
+
+
+define("DATABASE", "heroku_60b4fc996f0ab0b");
+define("SERVER", "us-cdbr-east-06.cleardb.net");
+define("USERNAME", "b2108b3fbdc536");
+define("PASSWD", "fd4e786c");
+
+// mysql://b2108b3fbdc536:fd4e786c@us-cdbr-east-06.cleardb.net/heroku_60b4fc996f0ab0b?reconnect=true
 
 ?>
